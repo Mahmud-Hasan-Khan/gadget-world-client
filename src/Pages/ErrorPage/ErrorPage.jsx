5e82c2af42c6c20463eb7063c8234e437bc9ef7b
Mahmud Hasan <mahmud.hasan@ymail.com>
2023-10-18 13:26:36 +0600

    error page implemented

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 
-
 const ErrorPage = () => {
     return (
-        <div>
-            <h1>Error</h1>
-            <Link to="/">Back to Home</Link>
+        <div className="flex items-center justify-center">
+            <div className='flex flex-col items-center relative'>
+                <Link className="absolute  top-2 md:top-[2%] lg:top-[3%]  bg-[#ad5cb4] font-medium text-white py-3 px-3 rounded-lg hover:bg-[#ac49b5] hover:shadow-lg" to="/" >Back To Home</Link>
+                <img className='object-cover w-screen h-screen' src="https://i.ibb.co/XJ0w6kW/404.jpg" alt="404" />
+            </div>
         </div>
     );
 };
